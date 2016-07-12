2519abab644871832f7468e62f86057db4694cd7
Jon Pugh <jon@thinkdrop.net>
2016-07-12 11:03:12 -0400

    Don't sleep! mysql_ready() function pings mysql, so the moment we are ready, we launch.

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -2,9 +2,20 @@
 
 HOSTNAME=`hostname --fqdn`
 
-sleep 5
+# Returns true once mysql can connect.
+mysql_ready() {
+	mysqladmin ping --host=database --user=root --password=MYSQL_ROOT_PASSWORD > /dev/null 2>&1
+}
+
+while !(mysql_ready)
+do
+   sleep 3
+   echo "waiting for mysql ..."
+done
+
+echo "========================="
 echo "Hostname: $HOSTNAME"
-echo "Waiting 5 seconds..."
+echo "Running: drush hostmaster-install"
 
 drush hostmaster-install $HOSTNAME --aegir_db_host=database --aegir_db_pass=$MYSQL_ROOT_PASSWORD --aegir_db_port=3306 --aegir_db_user=root --aegir_host=$HOSTNAME -y
 
