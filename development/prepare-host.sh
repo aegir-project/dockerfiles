aa5301d3328321556e1fa61f0815d2e0a97a1a83
Jon Pugh <jon@thinkdrop.net>
2016-07-12 13:59:30 -0400

    default to 3.x branch, it's a dev environment!

--- development/prepare-host.sh
+++ development/prepare-host.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-AEGIR_VERSION=7.x-3.6
+AEGIR_VERSION=7.x-3.x
 
 if [ ! -d aegir ]; then
   mkdir aegir
@@ -11,7 +11,7 @@ cd aegir
 
 # Build a full hostmaster frontend on the host with drush make, with working-copy option.
 if [ ! -d hostmaster-$AEGIR_VERSION ]; then
-   drush make http://cgit.drupalcode.org/provision/plain/aegir-release.make?h=$AEGIR_VERSION hostmaster-$AEGIR_VERSION --working-copy --no-gitinfofile
+   drush make http://cgit.drupalcode.org/provision/plain/aegir.make?h=$AEGIR_VERSION hostmaster-$AEGIR_VERSION --working-copy --no-gitinfofile
 #   cp hostmaster-$DEVMASTER_VERSION/sites/default/default.settings.php devmaster-$DEVMASTER_VERSION/sites/default/settings.php
 #   mkdir hostmaster-$AEGIR_VERSION/sites/devshop.site
    chmod 777 hostmaster-$AEGIR_VERSION/sites -R
