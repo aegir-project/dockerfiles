32690704bd2bca4a9f5c306e093a3fe99beb79f4
Jon Pugh <jon@thinkdrop.net>
2016-07-15 00:16:24 -0400

    Prepare test folder as writable if user can't write to it.

--- docker-entrypoint-tests.sh
+++ docker-entrypoint-tests.sh
@@ -6,7 +6,15 @@
 bash docker-entrypoint.sh || exit $?
 
 # Run some tests.
-cd /var/aegir/tests || exit $?
+echo "Preparing tests..."
+
+if [[ -w /var/aegir/tests ]];
+  cd /var/aegir/tests
+then
+  cp -rf /var/aegir/tests /var/aegir/tests-writable
+  cd /var/aegir/tests-writable
+fi
+
 composer update     || exit $?
 composer install    || exit $?
 bin/behat           || exit $?
