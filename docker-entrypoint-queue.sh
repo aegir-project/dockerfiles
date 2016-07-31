7ba16bc564fb803c39be47eae0b050d061c6717a
Jon Pugh <jon@thinkdrop.net>
2016-07-31 13:30:23 -0400

    Fixing queue entrypoint script, being more specific in output logs.

--- docker-entrypoint-queue.sh
+++ docker-entrypoint-queue.sh
@@ -2,16 +2,16 @@
 
 HOSTNAME=`hostname --fqdn`
 
-# Returns true once @hostmaster
+# Returns true once @hostmaster hosting_queued module is enabled.
 # Thanks to http://askubuntu.com/questions/697798/shell-script-how-to-run-script-after-mysql-is-ready
 hostmaster_ready() {
-    drush @hostmaster status > /dev/null 2>&1
+    drush @hostmaster pm-list --pipe --type=module --status=enabled --no-core hosting_queued | grep 'hosting_queued' > /dev/null 2>&1
 }
 
 while !(hostmaster_ready)
 do
    sleep 3
-   echo "waiting for hostmaster ..."
+   echo "waiting for Hostmaster Queue Daemon module to be enabled..."
 done
 
 echo "Hostmaster ready! running drush @hostmaster hosting-queued"
