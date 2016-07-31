1f00d8e747fa910ccede5fc14d09505bcf867276
Jon Pugh <jon@thinkdrop.net>
2016-07-31 14:10:36 -0400

    Adding more functions to wait for hostmaster queued to be TRULY ready... first we wait for drush alias to return, then we must wait for the pm-list command to be ready, then we use the pm-list command to wait for the hosting_queued module to be enabled.

--- docker-entrypoint-queue.sh
+++ docker-entrypoint-queue.sh
@@ -5,16 +5,39 @@ HOSTNAME=`hostname --fqdn`
 # Returns true once @hostmaster hosting_queued module is enabled.
 # Thanks to http://askubuntu.com/questions/697798/shell-script-how-to-run-script-after-mysql-is-ready
 hostmaster_ready() {
-    drush @hostmaster pm-list --pipe --type=module --status=enabled --no-core hosting_queued | grep 'hosting_queued' > /dev/null 2>&1
+    drush @hostmaster status > /dev/null 2>&1
+}
+
+function hostmaster_pm_list_ready() {
+    drush @hostmaster pm-list > /dev/null 2>&1
+}
+
+function hostmaster_queued_ready() {
+    drush @hostmaster pm-list --pipe --type=module --status=enabled --no-core | grep 'hosting_queued' &> /dev/null 2>&1
 }
 
 while !(hostmaster_ready)
 do
-   sleep 3
-   echo "waiting for Hostmaster Queue Daemon module to be enabled..."
+   sleep 5
+   echo "waiting for Hostmaster site to come online..."
 done
 
-echo "Hostmaster ready! running drush @hostmaster hosting-queued"
+echo "Hostmaster detected!"
+echo "waiting for Hostmaster Queue Daemon module to be enabled..."
+
+while !(hostmaster_pm_list_ready)
+do
+  echo "drush pm-list command failed... waiting."
+  sleep 5
+done
+
+while !(hostmaster_queued_ready)
+do
+  sleep 3
+  echo "waiting for Hostmaster Queue Daemon module to be enabled..."
+done
+
+echo "Hostmaster Queue ready! running drush @hostmaster hosting-queued"
 
 # Run whatever is the Docker CMD.
 drush @hostmaster hosting-queued -v
