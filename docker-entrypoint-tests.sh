7e5455869f171638285038eaccde45d93577048b
Jon Pugh <jon@thinkdrop.net>
2016-08-10 13:07:49 -0400

    Removing source code copy from tests entrypoint, adding to local makefile.

--- docker-entrypoint-tests.sh
+++ docker-entrypoint-tests.sh
@@ -12,17 +12,6 @@ fi
 # Prepare hostmaster
 bash docker-entrypoint.sh
 
-# Copy modules, themes and drush
-if [ -d /source/modules ]; then
-
-  # Place in sites/domainname.com/modules so that they override modules in distribution
-  cp -rf /source/modules/* /var/aegir/hostmaster-7.x-3.x/sites/aegir.local.computer/modules
-  cp -rf /source/themes/* /var/aegir/hostmaster-7.x-3.x/sites/aegir.local.computer/themes
-  cp -rf /source/drush/* /var/aegir/.drush/source
-
-fi
-
-
 # Run some tests.
 echo "Preparing tests..."
 
